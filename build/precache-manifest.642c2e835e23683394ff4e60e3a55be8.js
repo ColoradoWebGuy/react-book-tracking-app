8a1b3a24666716a40b48cba459df6ff44be9db63
Steven Schulte <steven@stvn.io>
2022-07-05 21:57:17 -0400

    Deploying to production

---
+++ build/precache-manifest.642c2e835e23683394ff4e60e3a55be8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d5c156f2323d08ed4a8",
+    "url": "/static/css/main.baa1b0f0.chunk.css"
+  },
+  {
+    "revision": "2d5c156f2323d08ed4a8",
+    "url": "/static/js/main.2d5c156f.chunk.js"
+  },
+  {
+    "revision": "34a28e6bfc8fd69e6e07",
+    "url": "/static/js/1.34a28e6b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "10a8696345f9c1c03402f988a01aeb90",
+    "url": "/static/media/arrow-back-hover.10a86963.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "cbb7da17452585221226b64cac7f7213",
+    "url": "/index.html"
+  }
+];
